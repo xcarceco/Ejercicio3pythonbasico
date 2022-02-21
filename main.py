08e6b269c5e8bb8df46d2572f5e8c43973babb71
xcarceco <xcarceco@gmail.com>
2022-02-21 23:56:24 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Para este ejercicio tenéis que crear un archivo main.py y
+# dentro tienes que crear variables que representen los siguientes datos de un contacto:
+# - Nombre
+# - Apellidos
+# - Edad
+# - Email
+# - Teléfono
+# - Casado (verdadero o falso)
+# - Con Hijos (verdadero o falso)
+# - Lista de amigos
+# - Películas vistas (diccionario con clave y valor. El valor será el título de la película)
+# Una vez hayas creado todas las variables, muéstralas por consola haciendo uso de la función print().
+
+Nombre='Nombre=Xavier\n'
+Apellidos='Apellidos=Crceren\n'
+Edad='Edad=31\n'
+Email='Email=xcarc@gmail.com\n'
+Telefono='Teléfono=645905666\n'
+Casado='Casado=falso\n'
+Con_hijos='Con hijos=falso\n'
+Lista_de_amigos=('Lista de amigos=Juan', 'Pepe', 'Emilio', 'Marc', 'Jorge\n')
+Peliculas_vistas={'Pelicula':'The Esentials'}
+
+print(Nombre,Apellidos,Edad,Email,Telefono,Casado,Con_hijos,Peliculas_vistas)
